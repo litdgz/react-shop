656e532022aeeb618b3174cc164565c933a1a9a6
Carlos Dominguez <litosandres@hotmail.com>
2022-08-23 11:51:28 -0600

    agregamos context al proyecto y usamos use context en ProductItem y Header para manejar el estado

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
+
+import AppContext from "@context/AppContext";
 import "@styles/ProductItem.scss";
 
 import addToShoppingCart from "@icons/bt_add_to_cart.svg";
 
 const ProductItem = ({ product }) => {
-  const [cart, setCart] = useState([]);
+  const { addToCart } = useContext(AppContext);
 
-  const handleClick = () => {
-    setCart([]);
+  const handleClick = (item) => {
+    addToCart(item);
   };
 
   return (
@@ -18,7 +20,7 @@ const ProductItem = ({ product }) => {
           <p>${product.price}</p>
           <p>{product.title}</p>
         </div>
-        <figure onClick={handleClick}>
+        <figure onClick={() => handleClick(product)}>
           <img src={addToShoppingCart} alt="" />
         </figure>
       </div>
